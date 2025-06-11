4d41ec35492604148961e86bb876a0148c2a2346
SLargo18 <slargo-b@DESKTOP-04F8OQO.localdomain>
2025-06-12 01:30:16 +0200

    initial struct

--- include/pipex.h
+++ include/pipex.h
@@ -3,9 +3,19 @@
 
 # include <unistd.h>
 # include <stdio.h>
-#
-#
-#
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
 
+typedef	struct s_pipex
+{
+	int		infle;
+	int		outfile;
+	int		pipefd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+}	t_pipex;
 
 #endif
