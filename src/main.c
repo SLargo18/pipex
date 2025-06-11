6b1bfa5a8c0fd9691ebddfad2d4c4a4ba14cec2f
SLargo18 <slargo-b@DESKTOP-04F8OQO.localdomain>
2025-06-12 01:47:57 +0200

    planing parse

---
+++ src/main.c
@@ -0,0 +1,10 @@
+
+#include "../include/pipex.h"
+
+int main(int argc, char **argv, char **envp)
+{
+	t_pipex	data;
+
+	if (!validate_args(argc, argv))
+		exit(EXIT_FAILURE);
+}
--- src/parse.c
+++ src/parse.c
@@ -1,6 +1,18 @@
 #include "../include/pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+void	ft_error(char *msg, int flag)
 {
-	
+	ft_putstr(msg);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	exit(flag);
+}
+
+int validate_args(int argc, char**argv)
+{
+	if (argc != 5)
+	{
+		write();
+		return (0);
+	}
 }
