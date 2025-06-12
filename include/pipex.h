4ed885e1bdd06b21b4dcb0c1ba369abec6488df6
SLargo18 <slargo-b@DESKTOP-04F8OQO.localdomain>
2025-06-12 20:30:13 +0200

    Extract PATH from envp and split by ':'

--- include/pipex.h
+++ include/pipex.h
@@ -14,10 +14,14 @@ typedef	struct s_pipex
 	int		infle;
 	int		outfile;
 	int		pipefd[2];
+	char	**paths;
+	char	**cmd1;
+	char	**cmd2;
 	pid_t	pid1;
 	pid_t	pid2;
 }	t_pipex;
 
 void	ft_error(char *msg, int flag);
+char	**ft_split(char const *s, char c);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,27 @@
 
 #include "../include/pipex.h"
 
+int validate_args(int argc, char**argv)
+{
+	if (argc != 5)
+	{
+		ft_error("ERROR: Invalid Arguments\n", 1);
+		return (0);
+	}
+	if (!argv[2][0] || !argv[3][0] )
+	{
+		ft_error("ERROR: Invalid Command\n", 1);
+		return (0);
+	}
+	return (1);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	t_pipex	data;
 
 	if (!validate_args(argc, argv))
 		exit(EXIT_FAILURE);
+	ft_pipex(argv, envp);
+	return(0);
 }
