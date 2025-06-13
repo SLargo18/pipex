8e954c9d226e9482d89f5cb9a431668cfc913407
SLargo18 <slargo-b@DESKTOP-04F8OQO.localdomain>
2025-06-13 02:49:47 +0200

    creating fork

--- src/utils_pipex.c
+++ src/utils_pipex.c
@@ -10,4 +10,27 @@ void	create_processes(t_pipex *data, char **argv, char **envp)
 {
 	open_file(data, argv);
 	data->pid1 = fork();
+	if (data->pid1 == -1)
+		is_error("Fork failed", 1, 0);
+	if (data->pid1 == 0)
+		first_child(data, argv, envp);
+	data->pid2 = fork();
+}
+
+void	child1(t_pipex *data, char **argv)
+{
+	close(data->pipefd[0]);
+	if (data->infile == -1)
+		is_error("Cannot open input file", 1, 0);
+	if (dup2(data->infile, STDIN_FILENO) == -1
+		|| (dup2(data->pipefd[1], STDIN_FILENO) == -1))
+		is_error ("dup2 failed", 1, 0);
+	close(data->infile);
+	close(data->pipefd[1]);
+}
+
+void	first_child(t_pipex *data, char **argv, char **envp)
+{
+	child1(data, argv);
+	execute_cmd(data->paths, data->cmd1, envp);
 }
